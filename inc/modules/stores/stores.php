8e63fce075f7a1ca90b2eb0e9d49e1378bc5ac18
manu-el <manue@ilch.de>
2013-10-05 10:11:05 +0200

    remove handler include it to store controller

--- inc/modules/stores/stores.php
+++ inc/modules/stores/stores.php
@@ -10,8 +10,37 @@ class Stores extends Controller {
     }
 
     function handle(Request $request) {
-        $template = new Template('stores/stores');
-        $template->set_ar('stores', $this->db->stores());
-        $template->display();
+        // TODO: add check to prevent cross site request forgery.
+        if ($request->is_post()) {
+            $this->handle_formular_submit($request);
+        } else {
+            $template = new Template('stores/stores');
+            $template->set_ar('stores', $this->db->stores());
+            $template->display();
+        }
+    }
+
+    private function handle_formular_submit(Request $request) {
+        if ($request->param_exists('new_name')) {
+            $this->db->insert($request->populate(array('new_name' => '')));
+        }
+
+        if ($request->param_exists('name')) {
+            foreach ($request->param('name') as $id => $name) {
+                $this->db->update($id, $name);
+
+                $new_number = $request->param('new_number');
+                $number = $request->param('number');
+                if (!empty($new_number[$id])) {
+                    $this->db->insert_yard($id, $new_number[$id]);
+                }
+
+                if (!empty($number[$id])) {
+                    foreach ($number[$id] as $yid => $ynumber) {
+                        $this->db->update_yard($yid, $ynumber);
+                    }
+                }
+            }
+        }
     }
 }
